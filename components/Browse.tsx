598027a16005d53fa6e5131685638b73573f193b
finnegan <finneganmcdoogle@gmail.com>
2023-02-28 21:14:21 +0700

    CIDs need to be kept on server

--- components/Browse.tsx
+++ components/Browse.tsx
@@ -8,12 +8,14 @@ import {
   InputLeftElement,
   InputGroup,
   Flex,
+  Image,
 } from "@chakra-ui/react";
 import useGetAlbumImages from "hooks/useGetAlbumImages";
 import useGetCIDs from "hooks/useGetCIDs";
 import { useAudioStore } from "store";
 
 const ImageGallery = ({ imageURLs }) => {
+  console.log("imageURLs", imageURLs);
   if (imageURLs) {
     return (
       // <Box maxW="70vw" mx="auto" overflowX="auto">
@@ -52,13 +54,119 @@ export default function Browse() {
 
   return (
     <Box flexWrap="nowrap" overflowY="auto">
-      <Button onClick={getCIDs} />
-      {/* <Flex direction="column" alignItems="center">
+      {/* <Button onClick={getCIDs} /> */}
+      <Flex direction="column" alignItems="center">
         <Text fontSize="3xl" fontWeight="bold" color="gray.600">
-          Jazz{" "}
+          Genres{" "}
         </Text>
-        {imgURLs ? <ImageGallery imageURLs={imgURLs.slice(0, 25)} /> : <></>}
-      </Flex> */}
+        {/* {imgURLs ? <ImageGallery imageURLs={imgURLs.slice(0, 25)} /> : <></>} */}
+        <ImageGallery
+          imageURLs={[
+            "/spaceMan.png",
+            "/musicMachine.png",
+            "/plagueFlute.png",
+            "/skywhales.png",
+            "/fluteTree.png",
+            "/monaLisa.png",
+            "/orbs.png",
+            "/ryoshiFlute1.png",
+            "/spaceMachine.png",
+            "/spaceMan.png",
+            "/musicMachine.png",
+            "/plagueFlute.png",
+            "/skywhales.png",
+            "/fluteTree.png",
+            "/monaLisa.png",
+            "/orbs.png",
+            "/ryoshiFlute1.png",
+            "/spaceMachine.png",
+          ]}
+        />
+      </Flex>
+      <Flex direction="column" alignItems="center">
+        <Text fontSize="3xl" fontWeight="bold" color="gray.600">
+          Genres{" "}
+        </Text>
+        {/* {imgURLs ? <ImageGallery imageURLs={imgURLs.slice(0, 25)} /> : <></>} */}
+        <ImageGallery
+          imageURLs={[
+            "/spaceMan.png",
+            "/musicMachine.png",
+            "/plagueFlute.png",
+            "/skywhales.png",
+            "/fluteTree.png",
+            "/monaLisa.png",
+            "/orbs.png",
+            "/ryoshiFlute1.png",
+            "/spaceMachine.png",
+            "/spaceMan.png",
+            "/musicMachine.png",
+            "/plagueFlute.png",
+            "/skywhales.png",
+            "/fluteTree.png",
+            "/monaLisa.png",
+            "/orbs.png",
+            "/ryoshiFlute1.png",
+            "/spaceMachine.png",
+          ]}
+        />
+      </Flex>
+      <Flex direction="column" alignItems="center">
+        <Text fontSize="3xl" fontWeight="bold" color="gray.600">
+          Genres{" "}
+        </Text>
+        {/* {imgURLs ? <ImageGallery imageURLs={imgURLs.slice(0, 25)} /> : <></>} */}
+        <ImageGallery
+          imageURLs={[
+            "/spaceMan.png",
+            "/musicMachine.png",
+            "/plagueFlute.png",
+            "/skywhales.png",
+            "/fluteTree.png",
+            "/monaLisa.png",
+            "/orbs.png",
+            "/ryoshiFlute1.png",
+            "/spaceMachine.png",
+            "/spaceMan.png",
+            "/musicMachine.png",
+            "/plagueFlute.png",
+            "/skywhales.png",
+            "/fluteTree.png",
+            "/monaLisa.png",
+            "/orbs.png",
+            "/ryoshiFlute1.png",
+            "/spaceMachine.png",
+          ]}
+        />
+      </Flex>
+      <Flex direction="column" alignItems="center">
+        <Text fontSize="3xl" fontWeight="bold" color="gray.600">
+          Genres{" "}
+        </Text>
+        {/* {imgURLs ? <ImageGallery imageURLs={imgURLs.slice(0, 25)} /> : <></>} */}
+        <ImageGallery
+          imageURLs={[
+            "/spaceMan.png",
+            "/musicMachine.png",
+            "/plagueFlute.png",
+            "/skywhales.png",
+            "/fluteTree.png",
+            "/monaLisa.png",
+            "/orbs.png",
+            "/ryoshiFlute1.png",
+            "/spaceMachine.png",
+            "/spaceMan.png",
+            "/musicMachine.png",
+            "/plagueFlute.png",
+            "/skywhales.png",
+            "/fluteTree.png",
+            "/monaLisa.png",
+            "/orbs.png",
+            "/ryoshiFlute1.png",
+            "/spaceMachine.png",
+          ]}
+        />
+      </Flex>
     </Box>
   );
 }
--- components/Library.tsx
+++ components/Library.tsx
@@ -7,12 +7,19 @@ import {
   Input,
   InputLeftElement,
   InputGroup,
+  Center,
+  Icon,
+  Flex,
 } from "@chakra-ui/react";
+import { HiOutlineWrenchScrewdriver } from "react-icons/hi2";
 
 export default function Library() {
   return (
-    <Box>
-      <Text>Your Uploads</Text>
-    </Box>
+    <Flex alignItems="center" justifyContent="center">
+      <VStack>
+        <Text fontSize="xxx-large">Coming Soon</Text>
+        <Icon as={HiOutlineWrenchScrewdriver} w="5vw" h="5vh" />
+      </VStack>
+    </Flex>
   );
 }
