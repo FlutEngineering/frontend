215c8adaf485e30602528a127e837dbb11c05302
finnegan <finneganmcdoogle@gmail.com>
2023-03-03 18:04:46 +0800

    added opensea logo

--- pages/api/v1/cids/index.ts
+++ pages/api/v1/cids/index.ts
@@ -1,42 +1,13 @@
-import { NextApiResponse, NextApiRequest } from "next";
-import { FilebaseClient, File } from "@filebase/client";
-import nc from "next-connect";
-import multer from "multer";
-import S3 from "aws-s3";
+// import { S3 } from "@aws-sdk/client-s3";
 
-const client = new FilebaseClient({
-  token: process.env.FILEBASE_API_KEY,
-});
-
-const handler = nc<NextApiRequest, NextApiResponse>({});
-
-handler.get(async (req, res) => {
-  // console.log("req =>", req);
-
-  const bucket = await client.bucket;
-
-  // console.log("bucket.list() =>", bucket.list());
-
-  console.log("bucket =>", client);
-  const config = {
-    bucketName: "fluteaudio",
-    region: "us-east-1",
-    accessKeyId: "9721FB07814959F0742C",
-    secretAccessKey: "5I5mvghn8WIY04di3PfLqCt0y4IPGJJTxAMeyM0T",
-  };
-  const S3Client = new S3(config);
-
-  const response = await fetch(`https://api.filebase.com/storage/${bucket}`, {
-    method: "GET",
-  });
-
-  return res.status(200).json({ bucket });
-});
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default handler;
+// const { endpoint, accessKey, secretKey, sessionToken } = s3Params;
+// const s3 = new S3({
+//   endpoint,
+//   signatureVersion: "v4",
+//   credentials: {
+//     accessKeyId: accessKey,
+//     secretAccessKey: secretKey,
+//     sessionToken,
+//   },
+//   region: "us-west-2",
+// });
