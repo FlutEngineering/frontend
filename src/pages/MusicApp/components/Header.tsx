8798aaff6bb24ceba3e774dc2f564a8a948a7b97
finnegan <finneganmcdoogle@gmail.com>
2023-03-11 22:11:28 -0500

    header1

--- src/pages/MusicApp/components/Header.tsx
+++ src/pages/MusicApp/components/Header.tsx
@@ -10,6 +10,7 @@ import {
   Button,
   Spacer,
   Flex,
+  Tooltip,
 } from "@chakra-ui/react";
 import {
   FaMediumM,
@@ -31,98 +32,100 @@ const Header: React.FC = () => {
     >
       <ConnectButton showBalance={false} />
       <Spacer />
-      <Link
-        as={RouterLink}
-        to="/"
-        key="Home"
-        _notFirst={{ ml: { base: 0, lg: 1 } }}
-        padding="5px"
-      >
-        <Icon
-          as={FaHome}
-          color="grey"
-          _hover={{ color: "black", transition: "0.4s" }}
-        />
-      </Link>
-      <Link
-        href="https://t.me/+BhXkKrFweoAzODcx"
-        key="Telegram"
-        _notFirst={{ ml: { base: 0, lg: 1 } }}
-        isExternal
-        padding="5px"
-      >
-        <Icon
-          as={FaTelegram}
-          color="grey"
-          _hover={{ color: "black", transition: "0.4s" }}
-        />
-      </Link>
-      <Link
-        href="https://medium.com/@TheMagicFlut"
-        key="Medium"
-        _notFirst={{ ml: { base: 0, lg: 1 } }}
-        isExternal
-        padding="5px"
-      >
-        <Icon
-          as={FaMediumM}
-          color="grey"
-          _hover={{ color: "black", transition: "0.4s" }}
-        />
-      </Link>
-      <Link
-        href="https://twitter.com/TheMagicFlut"
-        key="Twitter"
-        _notFirst={{ ml: { base: 0, lg: 1 } }}
-        isExternal
-        padding="5px"
-      >
-        <Icon
-          as={FaTwitter}
-          color="grey"
-          _hover={{ color: "black", transition: "0.4s" }}
-        />
-      </Link>
-      <Link
-        href="https://discord.gg/NXjGj9kHus"
-        key="Discord"
-        _notFirst={{ ml: { base: 0, lg: 1 } }}
-        isExternal
-        padding="5px"
-      >
-        <Icon
-          as={FaDiscord}
-          color="grey"
-          _hover={{ color: "black", transition: "0.4s" }}
-        />
-      </Link>
-      <Link
-        href="https://github.com/FlutEngineering"
-        key="Github"
-        _notFirst={{ ml: { base: 0, lg: 1 } }}
-        isExternal
-        padding="5px"
-      >
-        <Icon
-          as={FaGithub}
-          color="grey"
-          _hover={{ color: "black", transition: "0.4s" }}
-        />
-      </Link>
+      <HStack marginY="1rem">
+        <Link
+          as={RouterLink}
+          to="/"
+          key="Home"
+          _notFirst={{ ml: { base: 0, lg: 1 } }}
+          padding="5px"
+        >
+          <Icon
+            as={FaHome}
+            color="grey"
+            _hover={{ color: "black", transition: "0.4s" }}
+          />
+        </Link>
+        <Link
+          href="https://t.me/+BhXkKrFweoAzODcx"
+          key="Telegram"
+          _notFirst={{ ml: { base: 0, lg: 1 } }}
+          isExternal
+          padding="5px"
+        >
+          <Icon
+            as={FaTelegram}
+            color="grey"
+            _hover={{ color: "black", transition: "0.4s" }}
+          />
+        </Link>
+        <Link
+          href="https://medium.com/@TheMagicFlut"
+          key="Medium"
+          _notFirst={{ ml: { base: 0, lg: 1 } }}
+          isExternal
+          padding="5px"
+        >
+          <Icon
+            as={FaMediumM}
+            color="grey"
+            _hover={{ color: "black", transition: "0.4s" }}
+          />
+        </Link>
+        <Link
+          href="https://twitter.com/TheMagicFlut"
+          key="Twitter"
+          _notFirst={{ ml: { base: 0, lg: 1 } }}
+          isExternal
+          padding="5px"
+        >
+          <Icon
+            as={FaTwitter}
+            color="grey"
+            _hover={{ color: "black", transition: "0.4s" }}
+          />
+        </Link>
+        <Link
+          href="https://discord.gg/NXjGj9kHus"
+          key="Discord"
+          _notFirst={{ ml: { base: 0, lg: 1 } }}
+          isExternal
+          padding="5px"
+        >
+          <Icon
+            as={FaDiscord}
+            color="grey"
+            _hover={{ color: "black", transition: "0.4s" }}
+          />
+        </Link>
+        <Link
+          href="https://github.com/FlutEngineering"
+          key="Github"
+          _notFirst={{ ml: { base: 0, lg: 1 } }}
+          isExternal
+          padding="5px"
+        >
+          <Icon
+            as={FaGithub}
+            color="grey"
+            _hover={{ color: "black", transition: "0.4s" }}
+          />
+        </Link>
 
-      <Link
-        href="https://opensea.io/TheMagicFlut"
-        key="Opensea"
-        _notFirst={{ ml: { base: 0, lg: 1 } }}
-        isExternal
-        padding="5px"
-      >
-        <Icon
-          as={GiSailboat}
-          color="grey"
-          _hover={{ color: "black", transition: "0.4s" }}
-        />
-      </Link>
+        <Link
+          href="https://opensea.io/TheMagicFlut"
+          key="Opensea"
+          _notFirst={{ ml: { base: 0, lg: 1 } }}
+          isExternal
+          padding="5px"
+        >
+          <Icon
+            as={GiSailboat}
+            color="grey"
+            _hover={{ color: "black", transition: "0.4s" }}
+          />
+        </Link>
+      </HStack>
 
       <Box width="1vw" />
 
@@ -132,12 +135,14 @@ const Header: React.FC = () => {
         isExternal
         _hover={{}}
       >
-        <Button variant="outline" width="120px">
-          <HStack>
-            <Text textAlign="center">Buy on</Text>
-            <Image src={uniswapLogoUrl} w="2rem" />
-          </HStack>
-        </Button>
+        <Tooltip label="🌈 No Tax - Set a Low Slippage" placement="top">
+          <Button variant="outline" width="120px">
+            <HStack>
+              <Text textAlign="center">Buy on</Text>
+              <Image src={uniswapLogoUrl} w="2rem" />
+            </HStack>
+          </Button>
+        </Tooltip>
       </Link>
     </Flex>
   );
