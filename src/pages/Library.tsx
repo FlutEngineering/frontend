688edb359c6ed8ce8136b06174065dfa468f7c0e
finnegan <finneganmcdoogle@gmail.com>
2023-03-11 22:11:02 -0500

    changes

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,14 +1,31 @@
-import { VStack, Text, Icon, Flex } from "@chakra-ui/react";
+import { VStack, Text, Icon, Flex, Stack, Box } from "@chakra-ui/react";
 import { HiOutlineWrenchScrewdriver } from "react-icons/hi2";
+import { useTrackStore } from "~/store";
+import { useEffect } from "react";
+import AudioPlayer from "~/components/AudioPlayer";
+import { useAccount } from "wagmi";
 
 function Library(): JSX.Element {
+  const { address } = useAccount();
+  const { tracks, fetchTracks } = useTrackStore();
+
+  useEffect(() => {
+    fetchTracks();
+  }, []);
+
   return (
-    <Flex grow="1" alignItems="center" justifyContent="center">
-      <VStack>
-        <Text fontSize="xxx-large">Coming Soon</Text>
-        <Icon as={HiOutlineWrenchScrewdriver} w="5vw" h="5vh" />
-      </VStack>
-    </Flex>
+    <Box flexGrow="1">
+      <Text fontSize="3xl" fontWeight="bold" color="gray.600" marginY="1rem">
+        Your Uploads
+      </Text>
+      <Stack overflowY="scroll" spacing={2}>
+        {tracks.map((track) => {
+          if (track?.artistAddress === address) {
+            return <AudioPlayer track={track} key={track.title} />;
+          }
+        })}
+      </Stack>
+    </Box>
   );
 }
 
