5f005b4625e2becba17b70bd54045c4332220c11
finnegan <finneganmcdoogle@gmail.com>
2023-04-04 20:01:03 -0400

    feat: added Profile page and follow button

---
+++ src/pages/Profile/components/YourAccount.tsx
@@ -0,0 +1,30 @@
+import { Link as RouterLink } from "react-router-dom";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import {
+  Box,
+  Image,
+  HStack,
+  Link,
+  Icon,
+  Text,
+  Button,
+  Spacer,
+  Flex,
+  Tooltip,
+} from "@chakra-ui/react";
+import {
+  FaMediumM,
+  FaTwitter,
+  FaTelegram,
+  FaHome,
+  FaDiscord,
+  FaGithub,
+} from "react-icons/fa";
+import { GiSailboat } from "react-icons/gi";
+import uniswapLogoUrl from "~/assets/uniswap.svg";
+
+const YourAccount: React.FC = () => {
+  return <></>;
+};
+
+export default YourAccount;
