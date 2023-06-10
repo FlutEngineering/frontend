05c5b1ee4c80bbed91b183b76adfe8594a25c505
Finnegan <finneganmcdoogle@gmail.com>
2023-06-10 11:43:13 +0700

    feat: redirect after update

--- src/pages/TrackPage.tsx
+++ src/pages/TrackPage.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState, useCallback } from "react";
-import { Form, useLoaderData } from "react-router-dom";
+import { Form, useLoaderData, useNavigate } from "react-router-dom";
 import {
   HStack,
   VStack,
@@ -71,7 +71,8 @@ function TrackPage(): JSX.Element {
   const artist = useMemo(() => ens || track.artistAddress, [track, ens]);
   const image = ipfsCidToUrl(track.image);
   const { address } = useAccount();
-console.log('track',track)
+  const navigate = useNavigate();
+
   const [useTagStore] = useState<() => TagStore>(() =>
     create<TagStore>((set, get) => ({
       tags: [...new Set(track.tags)],
@@ -91,6 +92,7 @@ console.log('track',track)
   const [newTitle, setNewTitle] = useState(track.title || "");
   const [uploadState, setUploadState] = useState<UploadStates>("idle");
   const [errorMessage, setErrorMessage] = useState("");
+  const toast = useToast()
 
   const handleTrackUpdate = useCallback(async () => {
     setUploadState("initiated");
@@ -100,25 +102,24 @@ console.log('track',track)
     const formData = new FormData();
     formData.append("title", newTitle);
     tags.forEach((tag) => formData.append("tags", tag));
-    console.log("formData", formData);
     setUploadState("uploading");
 
     const response = await fetch(
       `${BACKEND_API_URL}/v1/tracks/${address}/${slug}`,
       {
         method: "PUT",
-        // headers: { "Content-Type": "application/json" },
+        headers: { "Content-Type": "application/json" },
         credentials: "include",
-        body: formData,
+        // body: formData,
+        body: JSON.stringify({ title: newTitle, tags })
       }
     );
-    console.log("response", response);
+    
     if (!response.ok) {
       setErrorMessage("Update error");
     }
 
     const json = await response.json();
-
     if (json.error) {
       setUploadState("error");
       setErrorMessage(json.error);
@@ -126,7 +127,7 @@ console.log('track',track)
     }
 
     setUploadState("success");
-    const toast = useToast()
+    navigate(`/${track?.artistAddress}/${json.track?.slug}`)
     toast({
       title: 'Update Successful',
       description: "Your track has been updated",
@@ -179,70 +180,71 @@ console.log('track',track)
       </HStack>
 
       {address === track.artistAddress ? (
-        <FormControl isInvalid={!newTitle.length}>
-          <Box
-            flex="1"
-            textAlign="left"
-            fontWeight="bold"
-            color="gray.600"
-            marginBottom='1.5rem'
-          >
-            Edit Track Details
-          </Box>
-          <FormLabel
-            fontSize="sm"
-            fontWeight="bold"
-            color="gray.600"
-          >
-            Update Title
-          </FormLabel>
-          
-          <Input
-            placeholder="Track title"
-            value={newTitle}
-            onChange={(event) => setNewTitle(event.target.value)}
-            maxLength={100}
-            minLength={1}
-            marginY={3}
-            isDisabled={isUploading}
+        <Box>
+            <Box
+              flex="1"
+              textAlign="left"
+              fontWeight="bold"
+              color="gray.600"
+              marginBottom='1.5rem'
+            >
+              Edit Track Details
+            </Box>
+            <FormLabel
+              fontSize="sm"
+              fontWeight="bold"
+              color="gray.600"
+            >
+              Update Title
+            </FormLabel>
             
-          />
-          <FormErrorMessage>Title is required.</FormErrorMessage>
-          <TagInput
-            size="sm"
-            label={
-              <FormLabel
-                display="inline-block"
-                verticalAlign="top"
-                fontSize="sm"
-                fontWeight="bold"
-                color="gray.600"
-              >
-                Update Tags
-              </FormLabel>
-            }
-            maxTags={MAX_TAGS}
-            tags={tags}
-            addTag={addTag}
-            removeTag={removeTag}
-            isInvalid={!tags.length}
-            isDisabled={isUploading}
-          />
-          <Button
-            marginY={3}
-            isLoading={uploadState === 'uploading'}
-            loadingText="Updating"
-            isDisabled={
-              !newTitle.length || !tags.length || tags.length < 3 || isUploading
-            }
-            onClick={() => handleTrackUpdate()}
-          >
-            Update
-          </Button>
-          <Text paddingLeft="2" fontSize="sm" color="red.500">
-            {errorMessage}
-          </Text>
-        </FormControl>
+            <Input
+              placeholder="Track title"
+              value={newTitle}
+              onChange={(event) => setNewTitle(event.target.value)}
+              maxLength={100}
+              minLength={1}
+              marginY={3}
+              isDisabled={isUploading}
+              
+            />
+            
+            
+            <TagInput
+              size="sm"
+              label={
+                <FormLabel
+                  display="inline-block"
+                  verticalAlign="top"
+                  fontSize="sm"
+                  fontWeight="bold"
+                  color="gray.600"
+                >
+                  Update Tags
+                </FormLabel>
+              }
+              maxTags={MAX_TAGS}
+              tags={tags}
+              addTag={addTag}
+              removeTag={removeTag}
+              isInvalid={!tags.length}
+              isDisabled={isUploading}
+            />
+            <Button
+              marginY={3}
+              isLoading={uploadState === 'uploading'}
+              loadingText="Updating"
+              isDisabled={
+                !newTitle.length || !tags.length || tags.length < 3 || isUploading
+              }
+              onClick={() => handleTrackUpdate()}
+            >
+              Update
+            </Button>
+            <Text paddingLeft="2" fontSize="sm" color="red.500">
+              {errorMessage}
+            </Text>
+          </Box>
       ) : (
         <></>
       )}
