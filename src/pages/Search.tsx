0406b32017c1add1969ca453bd2a914cd7835400
mrkot1359 <mrkot1359@proton.me>
2023-07-06 16:38:03 +0700

    chore: remove comment

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -23,68 +23,6 @@ function Search(): JSX.Element {
     }
   }, [selectedTag, fetchTracksByTag]);
 
-  // return (
-  //   <Grid
-  //     gridTemplateRows="auto auto minmax(0, 1fr)"
-  //     gridTemplateColumns="1fr"
-  //     gridTemplateAreas={`"header" "searchbar" "track-list"`}
-  //     width="100%"
-  //   >
-  //     <Text
-  //       gridArea="header"
-  //       fontSize="3xl"
-  //       fontWeight="bold"
-  //       color="gray.600"
-  //       marginY="1rem"
-  //     >
-  //       Browse All Uploads
-  //     </Text>
-  //     <Input
-  //       gridArea="searchbar"
-  //       variant="filled"
-  //       placeholder="Search by Name (Coming Soon)"
-  //       disabled
-  //       marginBottom="4"
-  //     />
-
-  //     {/* <Stack direction="row" spacing={4} paddingY="1" paddingBottom="4"> */}
-  //     {/*   <Button */}
-  //     {/*     leftIcon={<RiArrowUpDownFill />} */}
-  //     {/*     colorScheme="gray" */}
-  //     {/*     variant="outline" */}
-  //     {/*     disabled */}
-  //     {/*   > */}
-  //     {/*     Ranking */}
-  //     {/*   </Button> */}
-  //     {/*   <Button */}
-  //     {/*     leftIcon={<RiArrowUpDownFill />} */}
-  //     {/*     colorScheme="gray" */}
-  //     {/*     variant="outline" */}
-  //     {/*     disabled */}
-  //     {/*   > */}
-  //     {/*     Recent */}
-  //     {/*   </Button> */}
-  //     {/*   <Button */}
-  //     {/*     leftIcon={<RiArrowUpDownFill />} */}
-  //     {/*     colorScheme="gray" */}
-  //     {/*     variant="outline" */}
-  //     {/*     disabled */}
-  //     {/*   > */}
-  //     {/*     Unranked */}
-  //     {/*   </Button> */}
-  //     {/* </Stack> */}
-  //     <Box gridArea="track-list" alignSelf="stretch" overflowY="auto">
-  //       <Stack spacing={2} paddingBottom="2">
-  //         {tracks.map((track) => (
-  //           <AudioItem track={track} key={track.title} />
-  //         ))}
-  //       </Stack>
-  //     </Box>
-  //   </Grid>
-  // );
-
-  // -----------------------
-
   return (
     <Grid
       gridTemplateRows="auto auto minmax(0, 1fr)"
